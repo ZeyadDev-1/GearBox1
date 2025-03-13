9caa1d8c5f9f28cc4ce121f011b3bda963a28deb
ziad <zeyad.officemail@gmail.com>
2025-03-14 00:48:17 +0300

    test

--- GearBox/src/main/java/web/GearBox/GearBox.java
+++ GearBox/src/main/java/web/GearBox/GearBox.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//
 public class GearBox {
 
 	public static void main(String[] args) {
